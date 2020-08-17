65cd2b7d7243020cfb5c7d9f8509aeb1fd0607a1
nxf63550 <bhautik.ardeshana@nxp.com>
2020-08-17 18:12:16 +0530

    Strengthen the robustness of concurrent operations
    1. Ensure the concurrent access to mChannels in Terminal.java
       is protected by the same lock instance
    2. Move the remove operation of mSessions into synchronized block
       in method of Terminal.removeSession
    3. Add synchronous block protection to mChannels.add operation in
       SecureElementService.java

--- src/com/android/se/SecureElementService.java
+++ src/com/android/se/SecureElementService.java
@@ -309,7 +309,9 @@ public final class SecureElementService extends Service {
             Log.i(mTag, "Open basic channel success. Channel: "
                     + channel.getChannelNumber());
 
-            mChannels.add(channel);
+            synchronized (mLock) {
+                mChannels.add(channel);
+            }
             return channel.new SecureElementChannel();
         }
 
@@ -349,7 +351,9 @@ public final class SecureElementService extends Service {
             Log.i(mTag, "openLogicalChannel() Success. Channel: "
                     + channel.getChannelNumber());
 
-            mChannels.add(channel);
+            synchronized (mLock) {
+                mChannels.add(channel);
+            }
             return channel.new SecureElementChannel();
         }
     }
