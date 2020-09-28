7fe0f48e198793e77b62c71a6a0776642707a544
Ganesh Deva <ganesh.deva_1@nxp.com>
2020-09-28 20:49:01 +0530

    {11.0.0_r3}: Migrate to SecureElementStatsLog from StatsLog
    As part of statsd becoming a Mainline module in R, autogenerated
    StatsLog.write() calls are going away and replaced by *StatsLog.java
    that is autogenerated for each module.
    This CL adds autogenerated SecureElementStatsLog and replaces usages
    of StatsLog with SecureElementStatsLog.

--- src/com/android/se/Terminal.java
+++ src/com/android/se/Terminal.java
@@ -42,7 +42,6 @@ import android.se.omapi.ISecureElementReader;
 import android.se.omapi.ISecureElementSession;
 import android.se.omapi.SEService;
 import android.util.Log;
-import android.util.StatsLog;
 
 import com.android.se.SecureElementService.SecureElementSession;
 import com.android.se.internal.ByteArrayConverter;
@@ -136,9 +135,9 @@ public class Terminal {
                 if (mAccessControlEnforcer != null) {
                     mAccessControlEnforcer.reset();
                 }
-                StatsLog.write(
-                        StatsLog.SE_STATE_CHANGED,
-                        StatsLog.SE_STATE_CHANGED__STATE__DISCONNECTED,
+                SecureElementStatsLog.write(
+                        SecureElementStatsLog.SE_STATE_CHANGED,
+                        SecureElementStatsLog.SE_STATE_CHANGED__STATE__DISCONNECTED,
                         reason,
                         mName);
             } else {
@@ -153,9 +152,9 @@ public class Terminal {
                     // ignore
                 }
                 mDefaultApplicationSelectedOnBasicChannel = true;
-                StatsLog.write(
-                        StatsLog.SE_STATE_CHANGED,
-                        StatsLog.SE_STATE_CHANGED__STATE__CONNECTED,
+                SecureElementStatsLog.write(
+                        SecureElementStatsLog.SE_STATE_CHANGED,
+                        SecureElementStatsLog.SE_STATE_CHANGED__STATE__CONNECTED,
                         reason,
                         mName);
             }
@@ -166,9 +165,9 @@ public class Terminal {
         @Override
         public void serviceDied(long cookie) {
             Log.e(mTag, mName + " died");
-            StatsLog.write(
-                    StatsLog.SE_STATE_CHANGED,
-                    StatsLog.SE_STATE_CHANGED__STATE__HALCRASH,
+            SecureElementStatsLog.write(
+                    SecureElementStatsLog.SE_STATE_CHANGED,
+                    SecureElementStatsLog.SE_STATE_CHANGED__STATE__HALCRASH,
                     "HALCRASH",
                     mName);
             synchronized (mLock) {
@@ -258,9 +257,9 @@ public class Terminal {
             mSEHal.linkToDeath(mDeathRecipient, 0);
         }
         Log.i(mTag, mName + " was initialized");
-        StatsLog.write(
-                StatsLog.SE_STATE_CHANGED,
-                StatsLog.SE_STATE_CHANGED__STATE__INITIALIZED,
+        SecureElementStatsLog.write(
+                SecureElementStatsLog.SE_STATE_CHANGED,
+                SecureElementStatsLog.SE_STATE_CHANGED__STATE__INITIALIZED,
                 "INIT",
                 mName);
     }
@@ -436,9 +435,9 @@ public class Terminal {
         ChannelAccess channelAccess = null;
         if (packageName != null) {
             Log.w(mTag, "Enable access control on basic channel for " + packageName);
-            StatsLog.write(
-                    StatsLog.SE_OMAPI_REPORTED,
-                    StatsLog.SE_OMAPI_REPORTED__OPERATION__OPEN_CHANNEL,
+            SecureElementStatsLog.write(
+                    SecureElementStatsLog.SE_OMAPI_REPORTED,
+                    SecureElementStatsLog.SE_OMAPI_REPORTED__OPERATION__OPEN_CHANNEL,
                     mName,
                     packageName);
             try {
@@ -525,9 +524,9 @@ public class Terminal {
         ChannelAccess channelAccess = null;
         if (packageName != null) {
             Log.w(mTag, "Enable access control on logical channel for " + packageName);
-            StatsLog.write(
-                    StatsLog.SE_OMAPI_REPORTED,
-                    StatsLog.SE_OMAPI_REPORTED__OPERATION__OPEN_CHANNEL,
+            SecureElementStatsLog.write(
+                    SecureElementStatsLog.SE_OMAPI_REPORTED,
+                    SecureElementStatsLog.SE_OMAPI_REPORTED__OPERATION__OPEN_CHANNEL,
                     mName,
                     packageName);
             try {
