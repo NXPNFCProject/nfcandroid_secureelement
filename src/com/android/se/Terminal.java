3b74770201f2b60f9d687690c71c1198f639a78a
Pratapa Reddy Vaka <vakapratapa.reddy@nxp.com>
2020-10-29 13:25:54 +0530

    Copyright update

--- src/com/android/se/Terminal.java
+++ src/com/android/se/Terminal.java
@@ -36,7 +36,7 @@
 *  See the License for the specific language governing permissions and
 *  limitations under the License.
 *
-*  Copyright 2018-2020 NXP
+*  Copyright 2020 NXP
 *
 ******************************************************************************/
 package com.android.se;
