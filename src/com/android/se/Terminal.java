1ba7e478782546c458eb4e57191116fc39d7eb33
nxf31698 <suryaprakash.konduru@nxp.com>
2023-03-28 15:26:26 +0530

    Return true when secure_element HAL reset() is success
    This is what it used to do for HIDL, so we want to keep that behavior.
    
    Test: None
    Bug: None

--- src/com/android/se/Terminal.java
+++ src/com/android/se/Terminal.java
@@ -921,6 +921,7 @@ public class Terminal {
             try {
                 if (mAidlHal != null) {
                     mAidlHal.reset();
+                    return true;
                 } else {
                     byte status = mSEHal12.reset();
                     // Successfully trigger reset. HAL service should send onStateChange
