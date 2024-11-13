015ded5dbc36640cc40645d15d77349721c94a23
nxf56319 <sai.shwethas@nxp.com>
2024-11-13 15:01:13 +0530

    Aligned code to AOSP upto Oct 8th. (cherry picked from commit c6727568a4997729ebbf9531387ea38bff8ffca7)
    Change-Id: I5e726bf5cd8541e6ccce9742ec18f93a2e076f14

--- src/com/android/se/Terminal.java
+++ src/com/android/se/Terminal.java
@@ -80,6 +80,7 @@ public class Terminal {
     private static final boolean DEBUG = Build.isDebuggable();
     private static final int GET_SERVICE_DELAY_MILLIS = 4 * 1000;
     private static final int EVENT_GET_HAL = 1;
+    private static final int EVENT_NOTIFY_STATE_CHANGE = 2;
 
     private final int mMaxGetHalRetryCount = 5;
     private int mGetHalRetryCount = 0;
@@ -183,7 +184,7 @@ public class Terminal {
                         mName);
             }
 
-            sendStateChangedBroadcast(state);
+            mHandler.sendMessage(mHandler.obtainMessage(EVENT_NOTIFY_STATE_CHANGE, state));
         }
     }
 
@@ -249,6 +250,9 @@ public class Terminal {
                             Log.e(mTag, mName + " reach maximum retry count");
                         }
                     }
+                    break;
+                case EVENT_NOTIFY_STATE_CHANGE:
+                    sendStateChangedBroadcast((boolean) message.obj);
                     break;
                 default:
                     break;
